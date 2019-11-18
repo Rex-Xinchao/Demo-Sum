2a1ad6c04310e09c2e4265df19660cd06d74a016
Rex-Xinchao <13601935905@163.com>
2019-11-18 13:08:36 +0800

    ts模块

---
+++ src/components/typescript/test.ts
@@ -0,0 +1,30 @@
+const num: number = 123;
+
+function fn(arg: number, arg2: any): void {
+  console.log(arg, arg2)
+}
+
+fn(num, [1, 2, 3, 4]);
+
+interface IPerson {
+  name: string
+  age: number
+  family: string[]
+  sex: '男' | '女'
+}
+
+const person: IPerson = {
+  name: 'rex',
+  age: 25,
+  family: ['爹', '娘'],
+  sex: '男'
+}
+
+type IPerson2 = {
+  name: string
+  age: number
+  family: string[]
+  sex?: '男' | '女'
+}
+
+const person2: IPerson2 = person
